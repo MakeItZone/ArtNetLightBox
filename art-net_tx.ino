d8086f9daadbd0fc14334ebe6eff55e97eef2b08
Julian Rendell <juliangrendell@gmail.com>
2019-11-12 16:20:37 -0800

    fix for sending DMX data to a computer
    Yes, there was a missing character in the SSID!
    We need to look into the WiFi error codes and print some better debug messages!
    Also figured out how to receive ArtNET/DMX messages into QLC+ - set up ArtNET as an input, then go to virtual console. Add an (eg) slider. Double click on it and connect it to the appropriate ArtNet Universe and channel.
    Note: QLC+ only accepts FULL (512 entry) ArtNET packets! There maybe a setting somewhere to accept partial frames.

--- art-net_tx.ino
+++ art-net_tx.ino
@@ -8,7 +8,7 @@ int universe = 1;
 uint8_t data [4] = {50, 25, 10, 89};
 uint8_t size = 4;
 
-const char* ssid = "MakItZone";
+const char* ssid = "MakeItZone";
 const char* pwd = "26EBF7gv5tfV";
 const IPAddress ip(192, 168, 1, 200);
 const IPAddress gateway(192, 168, 1, 1);

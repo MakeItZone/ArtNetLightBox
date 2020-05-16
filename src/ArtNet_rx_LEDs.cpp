575d4a9be41a23c40363e662586e1e637577cfa2
DucksAndNetherwort <ducks.and.netherwort@gmail.com>
2020-05-16 13:09:34 -0700

    Implemented OTA updating

--- src/ArtNet_rx_LEDs.cpp
+++ src/ArtNet_rx_LEDs.cpp
@@ -12,6 +12,12 @@
 
 #include <Artnet.h>
 
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+//upload command: platformio run --target upload --upload-port [lightbox ip goes here]
+
 const int pwmMax = 255;
 
 int ledOnboard = 0; //onboard LED pin
@@ -159,6 +165,42 @@ void setup() {
       configFile.close();
       //end save
   }
+  //OTA segment start
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+  // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+//OTA segment end
     
     ip = WiFi.localIP();
     Serial.println(ip);
@@ -227,10 +269,15 @@ void loop(){
     doOnce = true;
     Serial.println(universe);
     Serial.println(Rin);
+    Serial.println(Rled);
     Serial.println(Gin);
+    Serial.println(Gled);
     Serial.println(Bin);
+    Serial.println(Gled);
     Serial.println(ledOnboardIn);
+    Serial.println(ledOnboard);
   }
   
     artnet.parse(); // check if artnet packet has come and execute callback
+    ArduinoOTA.handle(); //OTA stuffs
 }
